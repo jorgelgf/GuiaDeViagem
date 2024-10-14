5343b18e1baf2df4bf08bec0551ee763781263fc
jorgelgf <jlgurgelfernandes@gmail.com>
2024-10-14 09:32:04 -0300

    feat: inserida imagem no componente Home e ajustado

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,9 @@
-import { Grid2 } from '@mui/material'
+import { Box, Grid2, useTheme } from '@mui/material'
 import { Header, Layout } from '../../components'
 import { constants } from './constants'
 export const Home = () => {
 
+  const theme = useTheme();
   //
 
   //const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
@@ -23,7 +24,26 @@ export const Home = () => {
           alignItems='center'
           justifyContent='center'
         >
-          <img src={constants.images.main.src} alt={constants.images.main.alt} />
+          <img
+            width={theme.spacing(50)}
+            height={theme.spacing(50)}
+            loading="lazy"
+            src={constants.images.main.src}
+            alt={constants.images.main.alt}
+            style={{ borderRadius: '50%' }}
+          />
+          <Box
+            position='absolute'
+            zIndex={1}
+            marginRight={theme.spacing(35)}
+            marginTop={theme.spacing(50)}
+            bgcolor='#bdbdbd'
+            borderRadius='50%'
+            width={theme.spacing(20)}
+            height={theme.spacing(20)}
+          >
+
+          </Box>
         </Grid2>
         <Grid2
           height='100vh'
